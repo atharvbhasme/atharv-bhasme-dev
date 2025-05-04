f859a075e2201d1ff241638cbdfd7d5efc658fa3
Atharv Bhasme <atharvbhasme@gmail.com>
2025-05-05 01:17:52 +0530

    change the route in app

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/atharv-bhasme-dev/'
+  base: '/atharv-bhasme-dev/', 
+  plugins: [react()]
 })
