304d61e0050aba8b6933bd738e7820cd7277af9d
Atharv Bhasme <atharvbhasme@gmail.com>
2025-05-05 01:02:09 +0530

    change the base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/atharvbhasme/atharv-bhasme-dev'
+  base: '/atharv-bhasme-dev/'
 })
